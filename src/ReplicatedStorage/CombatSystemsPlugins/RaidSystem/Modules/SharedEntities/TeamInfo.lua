0222e1aeeade1aed19be1e55b01db70d82627665
monobrov1234 <79697582+monobrov1234@users.noreply.github.com>
2026-03-13 16:52:12 +0800

    Separate branches for configs and plugins

--- src/ReplicatedStorage/CombatSystemsPlugins/RaidSystem/Modules/SharedEntities/TeamInfo.lua
+++
@@ -1,8 +0,0 @@
-return {} :: {
-	Name: string,
-	LayoutOrder: number,
-	Color: Color3, -- color of the team
-	ImageAssetId: number, -- team image
-	StartPoints: number, -- team initial points
-	LinkedTeam: Team?, -- auto set on add
-}
